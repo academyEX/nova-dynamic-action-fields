8ec82728a307c7c9c439061797ae9a230444ae9c
Damian Mooyman <damian.mooyman@pixelfusion.co.nz>
2022-09-02 14:16:18 +1200

    fix: update fields method declaration

--- src/DynamicFieldAction.php
+++ src/DynamicFieldAction.php
@@ -4,26 +4,24 @@ namespace TuneZilla\DynamicActionFields;
 
 use Illuminate\Support\Collection;
 use Laravel\Nova\Http\Requests\ActionRequest;
+use Laravel\Nova\Http\Requests\NovaRequest;
 
 trait DynamicFieldAction
 {
-    /** @var ActionRequest|null */
-    public $actionRequest = null;
-
     final public function component()
     {
         return 'dynamic-action-field-modal';
     }
 
-    final public function fields()
+    /**
+     * @param ActionRequest $request
+     * @return array
+     */
+    final public function fields(NovaRequest $request)
     {
-        /** @var ActionRequest $actionRequest */
-        $actionRequest = $this->actionRequest ?? app(ActionRequest::class);
-
         $models = [];
-
-        if ($actionRequest->has('resources')) {
-            $actionRequest->chunks(100, function ($chunkedModels) use (&$models) {
+        if ($request->has('resources')) {
+            $request->chunks(100, function ($chunkedModels) use (&$models) {
                 /** @var \Illuminate\Database\Eloquent\Collection $chunkedModels */
                 $models = array_merge($models, $chunkedModels->all());
             });
